071c108951f0d0a34e6a47a1d49510d663b1091f
Juan Ignacio <zalbak87@gmail.com>
2026-05-09 23:10:51 -0300

    creacion de formulario reactivo contacto

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Frontend/src/app/components/about-us/about-us.ts
+++ Frontend/src/app/components/about-us/about-us.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../contact-form/contact-form';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
