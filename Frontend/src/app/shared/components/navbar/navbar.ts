bdb59be47ce6173a30467d9dd203e9dfe53defaf
agusalonzo <agustina.alonzo@mi.unc.edu.ar>
2026-05-30 21:24:17 -0300

    feature: agrego estilos globales. Agrego barra de busqueda

--- Frontend/src/app/shared/components/navbar/navbar.ts
+++ Frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  searchQuery: string = '';
+
+  constructor(private router: Router) {}
+
+  onSearch() {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/resultados'], {
+        queryParams: { q: this.searchQuery }
+      });
+      this.searchQuery = '';
+    }
+  }
+}
